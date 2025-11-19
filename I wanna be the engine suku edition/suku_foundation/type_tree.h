2674b5b1f5b80306377b09a2b9014066635c9c76
SfumatoCannon <3075964583@qq.com>
2025-11-19 09:13:41 +0800

    rename, add "type_tree.inl"

--- I wanna be the engine suku edition/suku_foundation/type_tree.h
+++ I wanna be the engine suku edition/suku_foundation/type_tree.h
@@ -30,81 +30,16 @@ namespace suku
 	private:
 		std::map<size_t, TypeNode*> typeMap_;
 	public:
-		template<typename T> void append()
-		{
-			if (typeMap_[typecode(T)] == nullptr)
-			{
-				TypeNode* newNode = new TypeNode;
-				newNode->code = typecode(T);
-				newNode->father = nullptr;
-				typeMap_[typecode(T)] = newNode;
-			}
-		}
+		template<typename T> void append();
 
-		template<typename Father, typename Son> void link()
-		{
-			TypeNode* fatherNode = typeMap_[typecode(Father)];
-			TypeNode* sonNode = typeMap_[typecode(Son)];
-			if (fatherNode != nullptr && sonNode != nullptr)
-			{
-				this->unlink<Son>();
-				sonNode->father = fatherNode;
-				fatherNode->child.push_back(sonNode);
-			}
-		}
+		template<typename Father, typename Son> void link();
 
-		template<typename Son> void unlink()
-		{
-			TypeNode* sonNode = typeMap_[typecode(Son)];
-			if (sonNode == nullptr)
-				return;
-			TypeNode* fatherNode = sonNode->father;
-			if (fatherNode == nullptr)
-				return;
-			for (auto iter = fatherNode->child.begin(); iter != fatherNode->child.end(); iter++)
-				if (*iter == sonNode)
-				{
-					fatherNode->child.erase(iter);
-					break;
-				}
-			sonNode->father = nullptr;
-		}
+		template<typename Son> void unlink();
 
-		template<typename T> std::list<TypeNode*> getChildList()
-		{
-			TypeNode* targetNode = typeMap_[typecode(T)];
-			if (targetNode != nullptr)
-				return targetNode->child;
-			else
-				return std::list<TypeNode*>();
-		}
+		template<typename T> std::list<TypeNode*> getImmediateChildrenList();
 
-		template<typename T> std::list<TypeNode*> getAllChildList()
-		{
-			TypeNode* rootNode = typeMap_[typecode(T)];
-			if (rootNode == nullptr)
-				return std::list<TypeNode*>();
-
-			std::list<TypeNode*> resultList;
-			std::list<TypeNode*> searchList;
-			std::map<size_t, bool> checkMap;
-			searchList.push_back(rootNode);
-			resultList.push_back(rootNode);
-			checkMap[rootNode->code] = true;
-			while (!searchList.empty())
-			{
-				auto currentNodeIterator = searchList.begin();
-				TypeNode* currentNode = *currentNodeIterator;
-				for (auto& sonNode : currentNode->child)
-					if (checkMap.find(sonNode->code) == checkMap.end())
-					{
-						searchList.push_back(sonNode);
-						resultList.push_back(sonNode);
-						checkMap[sonNode->code] = true;
-					}
-				searchList.erase(currentNodeIterator);
-			}
-			return resultList;
-		}
+		template<typename T> std::list<TypeNode*> getAllChildrenList();
 	};
 }
+
+#include "type_tree.inl"
