64740866aaf074247d061109ce00a7514568b41b
SfumatoCannon <3075964583@qq.com>
2025-12-09 15:25:39 +0800

    add Array2D in memory.h

--- I wanna be the engine suku edition/suku_foundation/memory.h
+++ I wanna be the engine suku edition/suku_foundation/memory.h
@@ -1,6 +1,32 @@
 #pragma once
+#include <utility>
+#include <vector>
+
 namespace suku::memory
 {
+	template<typename T>
+	class Array2D
+	{
+	public:
+		Array2D(size_t _rows, size_t _cols) : rows_(_rows), cols_(_cols), data_(_rows* _cols) {}
+		std::pair<size_t, size_t> getSize()const
+		{
+			return { rows_, cols_ };
+		}
+		T& operator[](size_t _rowIndex, size_t _colIndex)
+		{
+			return data_[_rowIndex * cols_ + _colIndex]
+		}
+		const T& operator[](size_t _rowIndex, size_t _colIndex)const
+		{
+			return data_[_rowIndex * cols_ + _colIndex];
+		}
+	private:
+		size_t rows_, cols_;
+		std::vector<T> data_;
+	};
+
+	// not recommended to use below functions
 	template<typename T> T** new_2d(UINT _rows, UINT _cols);
 	template<typename T> void delete_2d(T** _pointer, UINT _rows, UINT _cols);
 
