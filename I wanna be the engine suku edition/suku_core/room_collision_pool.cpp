b84e593ddc81a800659c09612c860c61379de7ee
SfumatoCannon <3075964583@qq.com>
2026-02-22 22:14:12 +0800

    RoomCollisionPool #1

--- I wanna be the engine suku edition/suku_core/room_collision_pool.cpp
+++ I wanna be the engine suku edition/suku_core/room_collision_pool.cpp
@@ -2,5 +2,26 @@
 
 namespace suku
 {
+    std::pair<int, int> RoomCollisionPool::getChunk(float _x, float _y)
+    {
+        return { static_cast<int>(_x / RoomCollisionPool::chunkSize),
+            static_cast<int>(_y / RoomCollisionPool::chunkSize) };
+    }
 
+    void RoomCollisionPool::addObject(Object* _obj)
+    {
+        std::pair<int, int> chunk = getChunk(_obj->x, _obj->y);
+        chunk_[chunk].push_back(_obj);
+        objectChunk_[_obj] = chunk;
+        objectIterator_[_obj] = --chunk_[chunk].end();
+		objectUpdateTag_[_obj] = false;
+    }
+
+    void RoomCollisionPool::removeObject(Object* _obj)
+    {
+		std::pair<int, int> chunk = objectChunk_[_obj];
+        chunk_[chunk].erase(objectIterator_[_obj]);
+        objectChunk_.erase(_obj);
+		objectIterator_.erase(_obj);
+    }
 }
--- I wanna be the engine suku edition/suku_core/room_collision_pool.h
+++ I wanna be the engine suku edition/suku_core/room_collision_pool.h
@@ -1,7 +1,24 @@
 #pragma once
-
+#include "room.h"
+#include "object.h"
+#include <map>
+#include <utility>
 
 namespace suku
 {
+	class RoomCollisionPool
+	{
+	public:
+		static constexpr int chunkSize = 128;
+		static std::pair<int, int> getChunk(float _x, float _y);
 
+		RoomCollisionPool() = default;
+		void addObject(Object* _obj);
+		void removeObject(Object* _obj);
+	private:
+		std::map<std::pair<int, int>, std::list<Object*>> chunk_;
+		std::map<Object*, std::pair<int, int>> objectChunk_;
+		std::map<Object*, std::list<Object*>::iterator> objectIterator_;
+		std::map<Object*, bool> objectUpdateTag_;
+	};
 }
