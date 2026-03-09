e6d116df68b5779fe3a36a18eb313b3682433d2f
SfumatoCannon <3075964583@qq.com>
2026-03-09 20:37:26 +0800

    add class SaveFile

--- I wanna be the engine suku edition/suku_foundation/save.cpp
+++ I wanna be the engine suku edition/suku_foundation/save.cpp
@@ -1,7 +1,6 @@
 #include "save.h"
 #include "../global_value.h"
 #include <ios>
-#include <fstream>
 
 namespace suku
 {
--- I wanna be the engine suku edition/suku_foundation/save.h
+++ I wanna be the engine suku edition/suku_foundation/save.h
@@ -5,6 +5,7 @@
 #include <map>
 #include <utility>
 #include <windows.h>
+#include <fstream>
 
 namespace suku
 {
@@ -31,6 +32,31 @@ namespace suku
 	private:
 		void suku_save_init();
 		SaveAssetGlobal() { suku_save_init(); }
+	};
+
+	class SaveFile
+	{
+	public:
+		SaveFile(String _path) : name_(L"save0"), path_(_path) {}	
+		SaveFile(String _name, String _path) : name_(_name), path_(_path) {}
+
+		void create()const;
+		void openForWrite();
+		bool tryOpenForWrite();
+		void openForRead();
+		bool tryOpenForRead();
+		void close();
+
+		void write(char* _ptrData, size_t _size)const;
+		void read(char* _ptrData, size_t _size)const;
+
+		void writeDataPtrMap(const std::map<unsigned long long, std::pair<char*, size_t>>& _dataPtrMap);
+		void readDataPtrMap(std::map<unsigned long long, std::pair<char*, size_t>>& _dataPtrMap);
+	private:
+		std::ofstream ofs_;
+		std::ifstream ifs_;
+		String name_;
+		String path_;
 	};
 
 	const wchar_t* AbsolutePath(const wchar_t* _relativePath);
