6907cc561826981bb80e62b275ad74a7375628a3
SfumatoCannon <3075964583@qq.com>
2026-03-26 11:37:19 +0800

    add class TextFactoryGlobal

--- I wanna be the engine suku edition/suku_draw/text.cpp
+++ I wanna be the engine suku edition/suku_draw/text.cpp
@@ -1,7 +1,19 @@
 #include "pch.h"
 #include "text.h"
+#include <dwrite.h>
 
 namespace suku
 {
 
+	namespace graphics
+	{
+		TextFactoryGlobal::TextFactoryGlobal()
+		{
+			DWriteCreateFactory(
+				DWRITE_FACTORY_TYPE_SHARED,
+				__uuidof(IDWriteFactory),
+				reinterpret_cast<IUnknown**>(pDWriteFactory_.GetAddressOf())
+			);
+		}
+	}
 }
--- I wanna be the engine suku edition/suku_draw/text.h
+++ I wanna be the engine suku edition/suku_draw/text.h
@@ -3,4 +3,24 @@
 namespace suku
 {
 
+
+	namespace graphics
+	{
+		using Microsoft::WRL::ComPtr;
+
+		class TextFactoryGlobal
+		{
+		public:
+			static IDWriteFactory* getDWriteFactory()
+			{
+				static TextFactoryGlobal instance;
+				return instance.pDWriteFactory_.Get();
+			}
+			TextFactoryGlobal(const TextFactoryGlobal&) = delete;
+			TextFactoryGlobal& operator=(const TextFactoryGlobal&) = delete;
+		private:
+			TextFactoryGlobal();
+			ComPtr<IDWriteFactory> pDWriteFactory_ = nullptr;
+		};
+	}
 }
