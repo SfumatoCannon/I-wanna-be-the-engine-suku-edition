2f63a1a90dbcdf14460cd1d4679b8a46b2a373a1
SfumatoCannon <3075964583@qq.com>
2026-05-08 12:34:28 +0800

    add transition func

--- I wanna be the engine suku edition/suku_core/transition.h
+++ I wanna be the engine suku edition/suku_core/transition.h
@@ -1,12 +1,28 @@
 #pragma once
 #include <functional>
+#include <cmath>
 
 namespace suku
 {
 	class TransitionFunction
 	{
-
+	public:
+		TransitionFunction() = default;
+		TransitionFunction(const std::function<double(double)>& _function) : function_(_function) {}
+	private:
+		std::function<double(double)> function_;
 	};
+
+	const TransitionFunction linear([](double t) { return t; });
+	const TransitionFunction easeInQuad([](double t) { return t * t; });
+	const TransitionFunction easeOutQuad([](double t) { return t * (2 - t); });
+	const TransitionFunction easeInOutQuad([](double t) { return t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t; });
+	const TransitionFunction easeInCubic([](double t) { return t * t * t; });
+	const TransitionFunction easeOutCubic([](double t) { return (--t) * t * t + 1; });
+	const TransitionFunction easeInOutCubic([](double t) { return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1; });
+	const TransitionFunction easeInExpo([](double t) { return t == 0 ? 0 : pow(2, 10 * (t - 1)); });
+	const TransitionFunction easeOutExpo([](double t) { return t == 1 ? 1 : 1 - pow(2, -10 * t); });
+	const TransitionFunction easeInOutExpo([](double t) { return t == 0 ? 0 : t == 1 ? 1 : t < 0.5 ? pow(2, 20 * t - 10) / 2 : (2 - pow(2, -20 * t + 10)) / 2; });
 
 	class Transition
 	{
