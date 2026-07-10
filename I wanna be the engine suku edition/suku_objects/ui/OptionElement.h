fc7ebeb043790370c65c414b2d6577b61d141602
SfumatoCannon <3075964583@qq.com>
2026-07-10 16:25:55 +0800

    remove infoOnChanging to keep simple

--- I wanna be the engine suku edition/suku_objects/ui/OptionElement.h
+++ I wanna be the engine suku edition/suku_objects/ui/OptionElement.h
@@ -14,49 +14,43 @@ namespace suku
 		OptionElement* next = nullptr;
 
 		template<typename T> OptionElement(
-			ConfigElement<T>& _bindedConfig, String _label, float _x, float _y, int _width, int _height, String _info = String(), String _infoOnChanging = String());
+			ConfigElement<T>& _bindedConfig, String _label, float _x, float _y, int _width, int _height, String _info = String());
 		template<typename T> OptionElement(ConfigElement<T>& _bindedConfig, String _label) : OptionElement(_bindedConfig, _label, 0, 0, 256, 32) {}
-		template<typename T> OptionElement(ConfigElement<T>& _bindedConfig, String _label, String _info, String _infoOnChanging = String()) : OptionElement(_bindedConfig, _label, 0, 0, 256, 32, _info, _infoOnChanging) {}
+		template<typename T> OptionElement(ConfigElement<T>& _bindedConfig, String _label, String _info) : OptionElement(_bindedConfig, _label, 0, 0, 256, 32, _info) {}
 
 		virtual void update() override { onUpdateFunc_(this); }
 		virtual bool onPaint() override { return onPaintFunc_(this); }
 	private:
 		friend class OptionPool;
 		String label_;
+		Text labelText_;
+		Text valueText_;
 		std::function<void(OptionElement*)> onUpdateFunc_;
 		std::function<bool(OptionElement*)> onPaintFunc_;
 		bool hasInfo_ = false;
-		bool hasInfoOnChanging_ = false;
 		String info_;
-		String infoOnChanging_;
+		Text infoText_;
 		bool isSelected_ = false;
 		int originalHeight_;
 	};
 
 	template<typename T>
-	inline OptionElement::OptionElement(ConfigElement<T>& _bindedConfig, String _label, float _x, float _y, int _width, int _height, String _info, String _infoOnChanging)
-		: UIElement(_x, _y, _width, _height), label_(_label), originalHeight_(_height)
+	inline OptionElement::OptionElement(ConfigElement<T>& _bindedConfig, String _label, float _x, float _y, int _width, int _height, String _info)
+		: UIElement(_x, _y, _width, _height), label_(_label), originalHeight_(_height),
+		labelText_("Consolas", _height / 2, TextAlign::MiddleLeft), valueText_("Consolas", _height / 2, TextAlign::MiddleRight), infoText_("Consolas", 16, TextAlign::TopLeft)
 	{
 		if (!_info.isEmpty())
 		{
 			hasInfo_ = true;
 			info_ = _info;
-		}
-
-		if (!_infoOnChanging.isEmpty())
-		{
-			hasInfoOnChanging_ = true;
-			infoOnChanging_ = _infoOnChanging;
+			infoText_.contentString = _info;
 		}
 
 		onUpdateFunc_ = [&_bindedConfig](OptionElement* _element)
 			{
 				if (_element->hasInfo_ && _element->isSelected_)
 				{
-					Text infoText("Consolas", 16, TextAlign::TopLeft);
-					infoText.contentString = _element->info_;
-
-					_element->height_ = _element->originalHeight_ + 8 + infoText.getContentHeight(_element->getWidth() - 16);
+					_element->height_ = _element->originalHeight_ + 8 + _element->infoText_.getContentHeight(_element->getWidth() - 16);
 				}
 				else
 					_element->height_ = _element->originalHeight_;
@@ -128,20 +122,21 @@ namespace suku
 				RectangleShape area(_element->getWidth(), _element->getHeight());
 				area.setFill(_element->isSelected_ ? Color(128, 128, 128, 0.5f) : Color(64, 64, 64, 0.5f));
 				area.paint(_element->x, _element->y, _element->transform);
-				Text text("Consolas", _element->originalHeight_ / 2, TextAlign::MiddleRight);
-				text.setBrush(Color(255, 255, 255));
-				text.contentString = std::to_wstring(_bindedConfig.value());
-				text.paint(_element->x + 8, _element->y + 8, _element->getWidth() - 16, _element->originalHeight_ - 16, graphics::createSolidColorBrush(Color(255, 255, 255)));
-				text.contentString = _element->label_;
-				text.setTextAlign(TextAlign::MiddleLeft);
-				text.paint(_element->x + 8, _element->y + 8, _element->getWidth() - 16, _element->originalHeight_ - 16, graphics::createSolidColorBrush(Color(255, 255, 255)));
+
+				// label text
+				_element->labelText_.setBrush(Color(255, 255, 255));
+				_element->labelText_.contentString = _element->label_;
+				_element->labelText_.paint(_element->x + 8, _element->y + 8, _element->getWidth() - 16, _element->originalHeight_ - 16, graphics::createSolidColorBrush(Color(255, 255, 255)));
+
+				// value text
+				_element->valueText_.setBrush(Color(255, 255, 255));
+				_element->valueText_.contentString = std::to_wstring(_bindedConfig.value());
+				_element->valueText_.paint(_element->x + 8, _element->y + 8, _element->getWidth() - 16, _element->originalHeight_ - 16, graphics::createSolidColorBrush(Color(255, 255, 255)));
+				
 				if (_element->hasInfo_ && _element->isSelected_)
 				{
-					Text infoText("Consolas", 16, TextAlign::TopLeft);
-					infoText.setBrush(Color(192, 192, 192));
-					infoText.contentString = _element->info_;
-					float height = infoText.getContentHeight(_element->getWidth() - 16);
-					infoText.paint(_element->x + 8, _element->y + _element->originalHeight_ + 4, _element->getWidth() - 16, height, graphics::createSolidColorBrush(Color(255, 255, 255)));
+					float height = _element->infoText_.getContentHeight(_element->getWidth() - 16);
+					_element->infoText_.paint(_element->x + 8, _element->y + _element->originalHeight_ + 4, _element->getWidth() - 16, height, graphics::createSolidColorBrush(Color(255, 255, 255)));
 				}
 				return false;
 			};
